3f64c58d676ba914d481909d40ea8eb96f3bc957
Oluwaseyi Ajayi <105451277+Oluwaseyiaja@users.noreply.github.com>
2022-06-24 05:57:49 -0700

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,11 @@
+alert('My name is Ajayi Oluwaseyi');
+alert('I am 6.3ft tall');
+alert('I am a Citizen of Nigeria');
+
+var name = ('My name is Ajayi Oluwaseyi');
+var height = ('6.3ft');
+var country = ('Nigeria');
+
+console.log(name);
+console.log(height);
+console.log(country);
